758d0692fa7741673e80a142753671d7e20ca9bb
Elliott D'Orazio <105451146+Elliott24@users.noreply.github.com>
2023-03-26 06:44:46 +0000

    Took code from old repl and added it to this one

---
+++ script.js
@@ -0,0 +1,7 @@
+let dark_prefered = window.matchMedia("(prefers-color-scheme: dark)")
+if(dark_prefered.matches) document.body.classList.add("dark")
+
+dark_prefered.addEventListener("change", () => {
+  if(dark_prefered.matches) document.body.classList.add("dark")
+  else document.body.classList.remove("dark")
+})
